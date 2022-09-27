3a9cccf2783000b6645d734030ce718c9273ab1f
C0B21055 <c0b2105559@edu.teu.ac.jp>
2022-09-27 18:23:36 +0900

    コメント #2

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -3,12 +3,12 @@ import string
 import time
 
 
-cha1=26
-cha2=10
-cha3=8
-cha4=2
+all_alp=26
+random_alp1=10
+random_alp2=8
+dell=2
 
-random_list = [random.choice(string.ascii_uppercase) for cha1 in range(cha2)]
+random_list = [random.choice(string.ascii_uppercase) for all_alp in range(random_alp1)]
 random_chr = "".join(random_list)
 
 print("対象文字：")
@@ -17,26 +17,27 @@ print(random_chr)
 time_sta = time.time()
 
 print("表示文字：")
-cha5=random.sample(random_list, cha3)
-print(cha5)
+
+new_random=random.sample(random_list, random_alp2)
+print(new_random)
 
 ans=input("欠損文字はいくつあるでしょうか？：")
 
-if int(ans)==cha4:
+if int(ans)==dell:
     print("正解です。それでは、具体的に欠損文字を1つずつ入力してください")
-    cha6=input("1つ目の文字を入力してください：")
-    cha7=input("2つ目の文字を入力してください：")
+    cha1=input("1つ目の文字を入力してください：")
+    cha2=input("2つ目の文字を入力してください：")
 
     time_end = time.time()
     times=time_end-time_sta
 
-    if (cha6 and cha7) in random_list:
-        if (cha6 or cha7)  in cha5:
+    if (cha1 and cha2) in random_list:
+        if (cha1 or cha2)  in new_random:
             print("不正解です。またチャレンジしてください。")
         else:
             print("正解です")
             print(f"タイムは{times}です。")
-            break
+            
 
 
 else:
