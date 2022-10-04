caf4a5fd880a701893a514e77c0c6db50d6063f3
C0B21055 <c0b2105559@edu.teu.ac.jp>
2022-10-04 16:47:16 +0900

    計算機改良版

--- ex2/calc1.py
+++ ex2/calc1.py
@@ -1,22 +1,79 @@
 import tkinter as tk
-import tkinter.messagebox as tkm
+from tkinter import ttk
+ 
 
-def click_number(event):
-    btn = event.widget
-    num = int(btn["text"])
-    tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
+BUTTON = [
+    ['', 'B', 'C', '/'],
+    ['7', '8', '9', '*'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['00', '0', '.', '=']
+]
+ 
+SYMBOL = ['+', '-', '*', '/']
 
+class CaluGui(object):
+    def __init__(self, app=None):
+        self.calc_str = ''
+    
+        app.geometry('300x500')
+ 
 
-root = tk.Tk() # 練習1
-root.geometry("300x500")
-
-r, c = 0, 0
-for i, num in enumerate(range(9,-1, -1), 1):
-    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
-    btn.bind("<1>", click_number)
-    btn.grid(row=r, column=c)
-    c += 1
-    if i%3 == 0:
-        r += 1
-        c = 0
-root.mainloop()
+        calc_frame = ttk.Frame(app, width=300, height=100) # 計算式と結果用のFrame
+        calc_frame.propagate(False) # サイズが固定される
+        calc_frame.pack(side=tk.TOP, padx=5, pady=5) # 余白の設定
+        button_frame = ttk.Frame(app, width=200, height=300) # 計算ボタン用のFrame
+        button_frame.propagate(False) # サイズが固定される
+        button_frame.pack(side=tk.BOTTOM) # 余白の設定
+ 
+        # Parts Setting
+        self.calc_var = tk.StringVar() # 計算式用の動的変数
+        self.ans_var = tk.StringVar() # 結果用の動的変数
+        calc_label = tk.Label(calc_frame, textvariable=self.calc_var, font=("",20)) # 計算式用のLabel
+        ans_label = tk.Label(calc_frame, textvariable=self.ans_var, font=("",30)) # 結果用のLabel
+        calc_label.pack(anchor=tk.E) # 右揃えで配置
+        ans_label.pack(anchor=tk.E) # 右揃えで配置
+ 
+        for y, row in enumerate(BUTTON, 1): # Buttonの配置
+            for x, num in enumerate(row):
+                button = tk.Button(button_frame, text=num, font=('', 15), width=6, height=3,bg="gray")
+                button.grid(row=y, column=x) # 列や行を指定して配置
+                button.bind('<Button-1>', self.click_button) # Buttonが押された場合
+    
+    def click_button(self, event):
+        check = event.widget['text'] # 押したボタンのCheck
+ 
+        if check == '=': # イコールの場合
+            if self.calc_str[-1:] in SYMBOL: # 記号の場合、記号よりも前で計算
+                self.calc_str = self.calc_str[:-1]
+ 
+            res = '= ' + str(eval(self.calc_str)) # eval関数の利用
+            self.ans_var.set(res)
+        elif check == 'C': # クリアの場合
+            self.calc_str = ''
+            self.ans_var.set('')
+        elif check == 'B': # バックの場合
+            self.calc_str = self.calc_str[:-1]
+        elif check in SYMBOL: # 記号の場合
+            if self.calc_str[-1:] not in SYMBOL and self.calc_str[-1:] != '':
+                self.calc_str += check
+            elif self.calc_str[-1:] in SYMBOL: # 記号の場合、入れ替える
+                self.calc_str = self.calc_str[:-1] + check
+        else: # 数字などの場合
+            self.calc_str += check
+ 
+        self.calc_var.set(self.calc_str)
+    
+ 
+ 
+def main():
+    # Window Setting
+    app = tk.Tk()
+    # Window size non resizable
+    app.resizable(width=False, height=False)
+    CaluGui(app)
+    # Display
+    app.mainloop() # Window をループで回すことで Widgit に対応できるようになる
+ 
+if __name__ == '__main__':
+    main()
