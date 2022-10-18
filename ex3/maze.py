ba2a6a68fc800356ed472a3e9208ca7f5a358d4a
C0B21055 <c0b2105559@edu.teu.ac.jp>
2022-10-18 14:21:18 +0900

    meiro-3

--- ex3/maze.py
+++ ex3/maze.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from xml.etree.ElementTree import tostring
 
 if __name__ == "__main__"
     root = tk.Tk()
@@ -6,5 +7,11 @@ if __name__ == "__main__"
 
     canv= tk.Canvas(root,width=1500,heigth=900,bg="black")
     canv.pack()
-    
+
+    tori = tk.PhotoImage(file = "fig/5.png")
+    cx,cy = 300, 400
+    canv.create_image(cx,cy,inage = tori, tag = "tori")
+
+    key = ""#現在押されているキーを表す
+
     root.mainloop()
