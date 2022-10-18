d3c4e04a35e0996f2ab47badc974f8dec113a1ab
C0B21055 <c0b2105559@edu.teu.ac.jp>
2022-10-18 14:11:37 +0900

    meiro-2

--- ex3/maze.py
+++ ex3/maze.py
@@ -3,5 +3,8 @@ import tkinter as tk
 if __name__ == "__main__"
     root = tk.Tk()
     root.title("迷えるこうかとん")
-    root.geometory("1500x900")
+
+    canv= tk.Canvas(root,width=1500,heigth=900,bg="black")
+    canv.pack()
+    
     root.mainloop()
