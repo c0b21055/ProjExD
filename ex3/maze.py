b8cd11d6d64a93c38e33c169a9afa8c00d15e397
C0B21055 <c0b2105559@edu.teu.ac.jp>
2022-10-18 14:42:03 +0900

    meiro-7

--- ex3/maze.py
+++ ex3/maze.py
@@ -4,6 +4,23 @@ def key_down(event):
     global key
     key = event.keysym
 
+def key_up(event):
+    global key
+    key= ""
+
+def main_proc():
+    global cx,cy
+    if key == "Up":
+        cy-= 20
+    if key == "Down":
+        cy += 20
+    if key == "Left":
+        cx -= 20
+    if key == "Right":
+        cx +=20
+    canv.coords("tori",cx,cy)
+    root.after(100,main_proc)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -22,5 +39,7 @@ if __name__ == "__main__":
 
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_down)
+
+    main_proc()
 
     root.mainloop()
