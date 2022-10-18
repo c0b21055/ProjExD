8f32b06c7c2ae818cedf25058caaf18874a2a876
C0B21055 <c0b2105559@edu.teu.ac.jp>
2022-10-18 15:20:23 +0900

    meiro-10

--- ex3/maze.py
+++ ex3/maze.py
@@ -1,3 +1,4 @@
+from importlib import machinery
 import tkinter as tk
 import maze_maker as mm
 
@@ -10,6 +11,7 @@ def key_up(event):
     key= ""
 
 def main_proc():
+    global mx,my
     global cx,cy
     if key == "Up":
         cy-= 20
@@ -19,6 +21,7 @@ def main_proc():
         cx -= 20
     if key == "Right":
         cx +=20
+    cx,cy = mx*100+50,my*100+50
     canv.coords("tori",cx,cy)
     root.after(100,main_proc)
 
@@ -31,18 +34,19 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()
 
+    maze_lst = mm.make_maze(15,9)
+    mm.show_maze(canv,maze_lst)
+
     tori = tk.PhotoImage(file="fig/5.png") 
     cx, cy = 300, 400
+    mx,my=1,1
     canv.create_image(cx, cy, image=tori, tag="tori")
 
 
     key = "" # 現在押されているキーを表す
 
     root.bind("<KeyPress>",key_down)
-    root.bind("<KeyRelease>",key_down)
-
-    maze_lst = mm.make_maze(15,9)
-    print(maze_lst)
+    root.bind("<KeyRelease>",key_up)
 
     main_proc()
 
