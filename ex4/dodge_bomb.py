e3f38966e7b79c87c1acc8edc768227f76375bce
C0B21055 <c0b2105559@edu.teu.ac.jp>
2022-10-25 14:27:49 +0900

    dodge_bomb

--- ex4/dodge_bomb.py
+++ ex4/dodge_bomb.py
@@ -2,31 +2,30 @@ import pygame as pg
 import sys
 
 def main():
+    # 練習1
     pg.display.set_caption("逃げろ！こうかとん")
-    scrn_sfc =  pg.display.set_mode((1600,900))
-
+    scrn_sfc = pg.display.set_mode((1600, 900))
     bg_sfc = pg.image.load("fig/pg_bg.jpg")
-    bg_sfc = bg_sfc.get_rect()
+    bg_rct = bg_sfc.get_rect()
 
-    clock = pg.time.Clock()
-
+    clock = pg.time.Clock() # 練習1
     while True:
-        scrn_sfc.blit(bg_sfc,bg_sfc)
-        pg.display.update()
+        scrn_sfc.blit(bg_sfc, bg_rct) # 練習2
+        pg.display.update() #練習2
         
-
-        for event in pg.event.get():
-            if event.type == pg.Quit:
+        for event in pg.event.get(): # 練習2
+            if event.type == pg.QUIT:
                 return
 
-        clock.time(0.2)
+
+        clock.tick(1000)
 
 
+   
 
-    
 
 if __name__ == "__main__":
-    pg.init()
-    main()
-    pg.quit()
+    pg.init() # 初期化
+    main() # ゲームの本体
+    pg.quit() # 初期化の解除
     sys.exit()
