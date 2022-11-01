2abf3c20be5860e5933cc924e81883cb95feb15e
C0B21055 <c0b2105559@edu.teu.ac.jp>
2022-11-01 16:58:04 +0900

    dodge

---
+++ ex5/dodge_bomb.py
@@ -0,0 +1,190 @@
+import pygame as pg
+import sys
+from random import randint
+
+
+
+class Screen:
+    def __init__(self, title, wh, bgimg):
+        pg.display.set_caption(title)
+        self.sfc = pg.display.set_mode(wh)
+        self.rct = self.sfc.get_rect()
+        self.bgi_sfc = pg.image.load(bgimg)
+        self.bgi_rct = self.bgi_sfc.get_rect()
+        
+    def blit(self):
+        self.sfc.blit(self.bgi_sfc, self.bgi_rct)
+
+
+class Bird:
+    key_delta = {
+        pg.K_UP:    [0, -1],
+        pg.K_DOWN:  [0, +1],
+        pg.K_LEFT:  [-1, 0],
+        pg.K_RIGHT: [+1, 0],
+    }
+
+    def __init__(self, img, zoom, xy):
+        sfc = pg.image.load(img)
+        self.sfc = pg.transform.rotozoom(sfc, 0, zoom)
+        self.rct = self.sfc.get_rect()
+        self.rct.center = xy
+
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr:Screen):
+        key_states = pg.key.get_pressed()
+        for key, delta in Bird.key_delta.items():
+            if key_states[key]:
+                self.rct.centerx += delta[0]
+                self.rct.centery += delta[1]
+                if check_bound(self.rct, scr.rct) != (+1, +1):
+                    self.rct.centerx -= delta[0]
+                    self.rct.centery -= delta[1]
+        self.blit(scr) 
+
+
+class Bomb:
+    def __init__(self, color, radius, vxy, scr:Screen):
+        self.sfc = pg.Surface((radius*2, radius*2)) # 空のSurface
+        self.sfc.set_colorkey((0, 0, 0)) # 四隅の黒い部分を透過させる
+        pg.draw.circle(self.sfc, color, (radius, radius), radius) # 爆弾用の円を描く
+        self.rct = self.sfc.get_rect()
+        self.rct.centerx = randint(0, scr.rct.width)
+        self.rct.centery = randint(0, scr.rct.height)
+        self.vx, self.vy = vxy
+    
+
+    def blit(self, scr:Screen):
+        
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr:Screen):
+        self.rct.move_ip(self.vx, self.vy)
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
+        self.blit(scr)
+
+
+class Bomb2:
+    def __init__(self, color, radius, vxy, scr:Screen):
+        self.sfc = pg.Surface((radius*2, radius*2)) # 空のSurface
+        self.sfc.set_colorkey((0, 0, 0)) # 四隅の黒い部分を透過させる
+        pg.draw.circle(self.sfc, color, (radius, radius), radius) # 爆弾用の円を描く
+        self.rct = self.sfc.get_rect()
+        self.rct.centerx = randint(0, scr.rct.width)
+        self.rct.centery = randint(0, scr.rct.height)
+        self.vx, self.vy = vxy
+    
+
+    def blit(self, scr:Screen):
+        
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr:Screen):
+        self.rct.move_ip(self.vx, self.vy)
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
+        self.blit(scr)
+
+
+class Bomb3:
+    def __init__(self, color, radius, vxy, scr:Screen):
+        self.sfc = pg.Surface((radius*2, radius*2)) # 空のSurface
+        self.sfc.set_colorkey((0, 0, 0)) # 四隅の黒い部分を透過させる
+        pg.draw.circle(self.sfc, color, (radius, radius), radius) # 爆弾用の円を描く
+        self.rct = self.sfc.get_rect()
+        self.rct.centerx = randint(0, scr.rct.width)
+        self.rct.centery = randint(0, scr.rct.height)
+        self.vx, self.vy = vxy
+    
+
+    def blit(self, scr:Screen):
+        scr.sfc.blit(self.sfc, self.rct)
+
+    def update(self, scr:Screen):
+        self.rct.move_ip(self.vx, self.vy)
+        yoko, tate = check_bound(self.rct, scr.rct)
+        self.vx *= yoko
+        self.vy *= tate
+        self.blit(scr)
+
+
+
+
+
+def check_bound(obj_rct, scr_rct):
+    """
+    obj_rct：こうかとんrct，または，爆弾rct
+    scr_rct：スクリーンrct
+    領域内：+1／領域外：-1
+    """
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: 
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: 
+        tate = -1
+    return yoko, tate
+
+
+def main():
+    # 練習1
+    scr = Screen("逃げろ！こうかとん", (1600, 900), "fig/pg_bg.jpg")
+
+    # 練習3
+    kkt = Bird("fig/6.png", 2.0, (900, 400))
+
+
+
+    # 練習5
+    r = randint(0, 255) 
+    g = randint(0, 255) 
+    b = randint(0, 255)
+    bkd = Bomb((r, g, b), 10, (+3, +3), scr)
+    r2 = randint(0, 255) 
+    g2 = randint(0, 255) 
+    b2 = randint(0, 255)
+    bkd2 = Bomb2((r2, g2, b2), 20, (+2, +2),scr)
+    r3 = randint(0, 255) 
+    g3 = randint(0, 255) 
+    b3 = randint(0, 255)
+    bkd3 = Bomb2((r3, g3, b3), 30, (+1, +1),scr)
+
+    clock = pg.time.Clock() # 練習1
+    while True:
+        scr.blit() # 練習2
+        
+        for event in pg.event.get(): # 練習2
+            if event.type == pg.QUIT:
+                return
+
+        # 練習4
+        kkt.update(scr)
+
+        # 練習7
+        bkd.update(scr)
+        bkd2.update(scr)
+        bkd3.update(scr)
+
+        # 練習8
+        if kkt.rct.colliderect(bkd.rct): # こうかとんrctが爆弾rctと重なったら
+            return
+
+        if kkt.rct.colliderect(bkd2.rct):
+            return
+
+        if kkt.rct.colliderect(bkd3.rct):
+            return
+
+        pg.display.update() #練習2
+        clock.tick(1000)
+
+
+if __name__ == "__main__":
+    pg.init() # 初期化
+    main()    # ゲームの本体
+    pg.quit() # 初期化の解除
+    sys.exit()
